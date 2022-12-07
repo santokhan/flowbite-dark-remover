f1c4e8926ffc2deece4c614cc48c35aa8401ebbd
Santo Khan <santokhan1999@gmail.com>
2022-12-07 14:25:28 +0600

    Flowbite Dark Remover initial commit for branch build

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-import DarkRemover from "./components/DarkRemover";
-
-function App() {
-  return <DarkRemover></DarkRemover>;
-}
-
-export default App;
